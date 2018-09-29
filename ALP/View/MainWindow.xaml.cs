50323c2c190e5da99cef9d21b8dd8904291a70e5
AUT\amocsari <amocsari@gmail.com>
2018-09-29 18:43:53 +0200

    naming corrections and initial window

--- ALP/View/MainWindow.xaml.cs
+++ ALP/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace APL
+namespace ALP.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
